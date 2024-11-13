758d5d584a1c5898f5b105fbfa1c1bdce6e74433
Harry MacCode <emarh.harry.code@gmail.com>
2024-11-13 23:55:11 +0100

    index.ts updated and dotenv installed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8900;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
